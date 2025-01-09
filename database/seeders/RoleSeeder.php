b37a73dae0b97cbe8a089d69ba4233d58b82400d
karsiary <karsiary@gmail.com>
2025-01-09 18:25:56 +0100

    seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -9,26 +9,26 @@ class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        Role::create({
-    "id": 1,
-    "name": "Administrator",
-    "slug": "admin",
-    "created_at": "2025-01-09 10:32:15",
-    "updated_at": "2025-01-09 10:32:15"
-});
-        Role::create({
-    "id": 2,
-    "name": "Nauczyciel",
-    "slug": "teacher",
-    "created_at": "2025-01-09 10:32:15",
-    "updated_at": "2025-01-09 10:32:15"
-});
-        Role::create({
-    "id": 3,
-    "name": "Student",
-    "slug": "student",
-    "created_at": "2025-01-09 10:32:15",
-    "updated_at": "2025-01-09 10:32:15"
-});
+        Role::create([
+            'id' => 1,
+            'name' => 'Administrator',
+            'slug' => 'admin',
+            'created_at' => '2025-01-09 10:32:15',
+            'updated_at' => '2025-01-09 10:32:15'
+        ]);
+        Role::create([
+            'id' => 2,
+            'name' => 'Nauczyciel',
+            'slug' => 'teacher',
+            'created_at' => '2025-01-09 10:32:15',
+            'updated_at' => '2025-01-09 10:32:15'
+        ]);
+        Role::create([
+            'id' => 3,
+            'name' => 'Student',
+            'slug' => 'student',
+            'created_at' => '2025-01-09 10:32:15',
+            'updated_at' => '2025-01-09 10:32:15'
+        ]);
     }
 }
